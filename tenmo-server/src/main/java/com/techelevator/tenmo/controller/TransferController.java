b47b4766979c6928baccc62a37a7a3d9680132b8
Amanda <amandarhartwick@gmail.com>
2021-03-04 13:30:51 -0500

    created controller classes

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -0,0 +1,13 @@
+package com.techelevator.tenmo.controller;
+
+@RestController
+@RequestMapping("/tenmo")
+
+public class TransferController {
+		private TransferDAO dao;
+		
+		public TransferController() {
+			this.dao = new TransferController();
+		}
+
+}
